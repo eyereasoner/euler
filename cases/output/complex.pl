925d03bb12b34309eeaac2eb08836044758e5a39
josd <josderoo@gmail.com>
2024-10-27 10:03:12 +0100

    refreshing

--- cases/output/complex.pl
+++ cases/output/complex.pl
@@ -1,6 +1,6 @@
 'https://eyereasoner.github.io/euler#exp'([-1,0],[0.5,0],[6.123233995736766e-17,1.0]).
-'https://eyereasoner.github.io/euler#exp'([2.718281828459045,0],[0,pi],[-1.0,1.2246467991473532e-16]).
-'https://eyereasoner.github.io/euler#log'([2.718281828459045,0],[-1,0],[0.0,3.141592653589793]).
+2.718281828459045 is 0+e,'https://eyereasoner.github.io/euler#exp'([2.718281828459045,0],[0,pi],[-1.0,1.2246467991473532e-16]).
+2.718281828459045 is 0+e,'https://eyereasoner.github.io/euler#log'([2.718281828459045,0],[-1,0],[0.0,3.141592653589793]).
 'https://eyereasoner.github.io/euler#log'([0,1],[0,1],[1.0,0.0]).
 'https://eyereasoner.github.io/euler#sin'([1.570796326794897,1.316957896924817],[2.0000000000000004,-6.631275506809351e-16]).
 'https://eyereasoner.github.io/euler#cos'([0,-1.316957896924817],[2.0000000000000004,0.0]).
