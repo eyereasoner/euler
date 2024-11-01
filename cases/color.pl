13a88bc1c6cb20a69661fa834b08f0d004c027a5
josd <josderoo@gmail.com>
2024-11-01 13:46:41 +0100

    refreshing

--- cases/color.pl
+++ cases/color.pl
@@ -13,36 +13,34 @@ places([[Place, Color]|Tail]) :-
     member(Color, [red, green, blue, yellow]),
     \+ (member([Neighbour, Color], Tail), member(Neighbour, Neighbours)).
 
-% test data
 % map of European Union
-neighbours(austria, [czech_republic, germany, hungary, italy, slovenia, slovakia]).
-neighbours(belgium, [france, netherlands, luxemburg, germany, united_kingdom]).
-neighbours(bulgaria, [romania, greece]).
-neighbours(croatia, [slovenia, hungary]).
-neighbours(cyprus, [greece]).
-neighbours(czech_republic, [germany, poland, slovakia, austria]).
-neighbours(denmark, [germany, sweden]).
-neighbours(estonia, [finland, latvia, lithuania]).
-neighbours(finland, [estonia, sweden]).
-neighbours(france, [spain, belgium, luxemburg, germany, italy, united_kingdom]).
-neighbours(germany, [netherlands, belgium, luxemburg, denmark, france, austria, poland, czech_republic]).
-neighbours(greece, [bulgaria, cyprus]).
-neighbours(hungary, [austria, slovakia, romania, croatia, slovenia]).
-neighbours(ireland, [united_kingdom]).
-neighbours(italy, [france, austria, slovenia]).
-neighbours(latvia, [estonia, lithuania]).
-neighbours(lithuania, [estonia, latvia, poland]).
+neighbours(belgium, [france, netherlands, luxemburg, germany]).
+neighbours(netherlands, [belgium, germany]).
 neighbours(luxemburg, [belgium, france, germany]).
-neighbours(malta, []).
-neighbours(netherlands, [belgium, germany, united_kingdom]).
-neighbours(poland, [germany, czech_republic, slovakia, lithuania]).
+neighbours(france, [spain, belgium, luxemburg, germany, italy]).
+neighbours(germany, [netherlands, belgium, luxemburg, denmark, france, austria, poland, czech_republic]).
+neighbours(italy, [france, austria, slovenia]).
+neighbours(denmark, [germany]).
+neighbours(ireland, []).
+neighbours(greece, [bulgaria]).
+neighbours(spain, [france, portugal]).
 neighbours(portugal, [spain]).
-neighbours(romania, [hungary, bulgaria]).
+neighbours(austria, [czech_republic, germany, hungary, italy, slovenia, slovakia]).
+neighbours(sweden, [finland]).
+neighbours(finland, [sweden]).
+neighbours(cyprus, []).
+neighbours(malta, []).
+neighbours(poland, [germany, czech_republic, slovakia, lithuania]).
+neighbours(hungary, [austria, slovakia, romania, croatia, slovenia]).
+neighbours(czech_republic, [germany, poland, slovakia, austria]).
 neighbours(slovakia, [czech_republic, poland, hungary, austria]).
 neighbours(slovenia, [austria, italy, hungary, croatia]).
-neighbours(spain, [france, portugal]).
-neighbours(sweden, [finland, denmark]).
-neighbours(united_kingdom, [ireland, netherlands, belgium, france]).
+neighbours(estonia, [latvia]).
+neighbours(latvia, [estonia, lithuania]).
+neighbours(lithuania, [latvia, poland]).
+neighbours(bulgaria, [romania, greece]).
+neighbours(romania, [hungary, bulgaria]).
+neighbours(croatia, [slovenia, hungary]).
 
 % query
 query('https://eyereasoner.github.io/euler#color'(map1, _ANSWER)).
